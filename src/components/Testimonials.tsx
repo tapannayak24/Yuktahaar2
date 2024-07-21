1003a2f28a6edd5dec01c2cac16d39d0b5d3f886
Achal Agrawal <achal272@gmail.com>
2024-07-21 20:55:31 +0530

    Minor dark mode changes

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,12 +4,14 @@ import { Container } from "@/components/Container";
 export const Testimonials = () => {
   return (
     <Container>
-      <h2 className="text-3xl font-bold text-center mb-10">
+      <h2 className="text-3xl font-bold text-center mb-10 dark:text-white">
         What They Say About Us
       </h2>
       <div className="grid gap-10 lg:grid-cols-2">
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <h3 className="text-xl font-semibold mb-4">Satisfied Consumers</h3>
+        <div className="bg-white dark:bg-lime-950 p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-semibold mb-4 dark:text-white">
+            Satisfied Consumers
+          </h3>
           <TestimonialCard
             content="Thanks to Yuktahaar for introducing us with people who care for people and environment instead of minting money......"
             name="Mrs. Vrushali Bhalerao"
@@ -22,17 +24,19 @@ export const Testimonials = () => {
             content="...Our families consider ourselves blessed to have access to such a platform, where we can procure authentic organic produce and savor the true richness of nature...."
             name="Mrs. Preeti Iyer"
           />
-          <p className="mt-4 text-green-600">
+          <p className="mt-4 text-green-600 dark:text-green-400">
             more than 100 families trust Yuktahaar.....
           </p>
         </div>
-        <div className="bg-green-50 p-6 rounded-lg shadow-md">
-          <h3 className="text-xl font-semibold mb-4">Trusted Producers</h3>
+        <div className="bg-green-50 dark:bg-green-950 p-6 rounded-lg shadow-md">
+          <h3 className="text-xl font-semibold mb-4 dark:text-white">
+            Trusted Producers
+          </h3>
           <TestimonialCard
             content="Proud to be part of yuktahaar community as it has got strong basis of thought and gives importance to relationships ultimately leading to a feeling of extended family..."
             name="Mandar Deshpande: Organic Farmer, Wardha"
           />
-          <p className="mt-4 text-green-600">
+          <p className="mt-4 text-green-600 dark:text-green-400">
             more than 50 producers trust Yuktahaar...
           </p>
         </div>
@@ -49,8 +53,8 @@ interface TestimonialCardProps {
 function TestimonialCard({ content, name }: TestimonialCardProps) {
   return (
     <div className="mb-6">
-      <p className="text-gray-700 mb-2">{content}</p>
-      <p className="text-right font-medium">- {name}</p>
+      <p className="text-gray-700 dark:text-gray-300 mb-2">{content}</p>
+      <p className="text-right font-medium dark:text-gray-400">- {name}</p>
     </div>
   );
 }
