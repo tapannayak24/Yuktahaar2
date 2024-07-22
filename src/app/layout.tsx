f165f597d1c233ba15e411633fe4f9c65ffc3c14
Achal Agrawal <achalxyz@gmail.com>
2024-07-22 17:23:51 +0530

    fix dark mode switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <div>{children}</div>
           <Footer />
